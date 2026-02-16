ff7e9de2efda343abd0b40a6fbaaa7c7b09c6807
Erick <erickjo10x@gmail.com>
2026-02-16 14:47:20 +0100

    feat: add JwtGuard, JwtRefreshGuard, and RolesGuard for enhanced authentication and authorization

---
+++ src/common/guards/roles.guard.ts
@@ -0,0 +1,33 @@
+import {
+  CanActivate,
+  ExecutionContext,
+  ForbiddenException,
+  Injectable,
+} from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { UserRole } from '../enums/user-role.enum';
+
+@Injectable()
+export class RolesGuard implements CanActivate {
+  constructor(private reflector: Reflector) {}
+
+  canActivate(context: ExecutionContext): boolean {
+    const requiredRoles = this.reflector.get<UserRole[]>(
+      'roles',
+      context.getHandler(),
+    );
+    if (!requiredRoles) {
+      return true;
+    }
+    const request = context.switchToHttp().getRequest();
+    const user = request.user;
+
+    if (!user || !requiredRoles.includes(user.role)) {
+      throw new ForbiddenException(
+        'No tienes permiso para acceder a este recurso',
+      );
+    }
+
+    return true;
+  }
+}

29e06eb95d1caf825933446fae06e7cf5fbd1458
Erick <erickjo10x@gmail.com>
2026-02-17 11:05:27 +0100

    feat: add JwtRefreshStrategy and JwtStrategy for enhanced JWT handling and validation

---
+++ src/common/strategies/jwt-refresh.strategy.ts
@@ -0,0 +1,52 @@
+import { ConfigService } from '@nestjs/config';
+import { PassportStrategy } from '@nestjs/passport';
+import { Strategy as CustomStrategy } from 'passport-custom';
+import * as jose from 'jose';
+import { TokenType } from '../enums/token-type.enum';
+import { UnauthorizedException } from '@nestjs/common';
+import { JwtPayload } from '../interfaces/jwt-payload.interface';
+
+export class JwtRefreshStrategy extends PassportStrategy(
+  CustomStrategy,
+  'jwt-refresh',
+) {
+  private secret: Uint8Array;
+
+  constructor(private configService: ConfigService) {
+    super();
+
+    const secretString = this.configService.get<string>('JWT_REFRESH_SECRET');
+    if (!secretString) {
+      throw new Error('JWT_REFRESH_SECRET variable de entorno no definida');
+    }
+    this.secret = new TextEncoder().encode(secretString);
+  }
+
+  async validate(req: any): Promise<any> {
+    const token = req.body?.refreshToken;
+
+    if (!token) {
+      throw new Error('Refresh token no proporcionado');
+    }
+
+    try {
+      const { payload } = await jose.jwtVerify(token, this.secret);
+      const jwtPayload = payload as unknown as JwtPayload;
+
+      if (jwtPayload.type !== TokenType.REFRESH) {
+        throw new UnauthorizedException('Token no es de tipo REFRESH');
+      }
+
+      return {
+        id: jwtPayload.id,
+        email: jwtPayload.email,
+        role: jwtPayload.role,
+      };
+    } catch (error: any) {
+      if (error.message.includes('Token is expired')) {
+        throw new UnauthorizedException('Refresh token expirado');
+      }
+      throw new UnauthorizedException('Refresh token inválido');
+    }
+  }
+}
---
+++ src/common/strategies/jwt.strategy.ts
@@ -0,0 +1,53 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { PassportStrategy } from '@nestjs/passport';
+import { Strategy as CustomStrategy } from 'passport-custom';
+import * as jose from 'jose';
+import { JwtPayload } from '../interfaces/jwt-payload.interface';
+import { TokenType } from '../enums/token-type.enum';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(CustomStrategy, 'jwt') {
+  private secret: Uint8Array;
+
+  constructor(private configService: ConfigService) {
+    super();
+
+    const secretString = this.configService.get<string>('JWT_SECRET');
+    if (!secretString) {
+      throw new Error('JWT_SECRET variable de entorno no definida');
+    }
+    this.secret = new TextEncoder().encode(secretString);
+  }
+
+  async validate(req: any): Promise<any> {
+    const authHeader = req.headers?.authorization || '';
+    const token = authHeader.startsWith('Bearer ') ? authHeader.slice(7) : null;
+
+    if (!token) {
+      throw new UnauthorizedException(
+        'Token de autenticación no proporcionado',
+      );
+    }
+
+    try {
+      const { payload } = await jose.jwtVerify(token, this.secret);
+      const jwtPayload = payload as unknown as JwtPayload;
+
+      if (jwtPayload.type !== TokenType.ACCESS) {
+        throw new UnauthorizedException('Tipo de token no válido');
+      }
+
+      return {
+        id: jwtPayload.id,
+        email: jwtPayload.email,
+        role: jwtPayload.role,
+      };
+    } catch (error: any) {
+      if (error.message.includes('Token is expired')) {
+        throw new UnauthorizedException('Token de autenticación expirado');
+      }
+      throw new UnauthorizedException('Token de autenticación no válido');
+    }
+  }
+}

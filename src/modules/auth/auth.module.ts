99406b90e2b5b5f93b2945970e426d8644948446
Erick <erickjo10x@gmail.com>
2026-02-18 15:53:28 +0100

    feat: create AuthModule with necessary imports, controllers, and providers

---
+++ src/modules/auth/auth.module.ts
@@ -0,0 +1,23 @@
+import { Module } from '@nestjs/common';
+import { PassportModule } from '@nestjs/passport';
+import { UsersModule } from '../users/users.module';
+import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+import { RefreshTokenRepository } from './repositories/refresh-token.repository';
+import { TokenService } from 'src/core/token.service';
+import { JwtStrategy } from 'src/common/strategies/jwt.strategy';
+import { JwtRefreshStrategy } from 'src/common/strategies/jwt-refresh.strategy';
+
+@Module({
+  imports: [PassportModule.register({ defaultStrategy: 'jwt' }), UsersModule],
+  controllers: [AuthController],
+  providers: [
+    AuthService,
+    RefreshTokenRepository,
+    TokenService,
+    JwtStrategy,
+    JwtRefreshStrategy,
+  ],
+  exports: [UsersModule],
+})
+export class AuthModule {}

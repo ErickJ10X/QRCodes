f1e5cfb1cbf53a61f2826d64b6360171604e5232
Erick <erickjo10x@gmail.com>
2026-02-18 16:30:38 +0100

    feat: update CurrentUser decorator to use IAuthenticatedRequest for user type

--- src/common/decorators/current-user.decorator.ts
+++ src/common/decorators/current-user.decorator.ts
@@ -1,8 +1,7 @@
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
-import { IRequestUser } from '../interfaces/request-user.interface';
-
+import { IAuthenticatedRequest } from '../interfaces/authenticated-request.interface';
 export const CurrentUser = createParamDecorator(
-  (data: unknown, ctx: ExecutionContext): IRequestUser => {
+  (data: unknown, ctx: ExecutionContext): IAuthenticatedRequest['user'] => {
     const request = ctx.switchToHttp().getRequest();
     return request.user;
   },

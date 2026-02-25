f4bde694aa25ce9580dc40ad73be2868b04e1d22
Erick <erickjo10x@gmail.com>
2026-02-25 20:18:27 +0100

    refactor: simplify GlobalAuthGuard by extending AuthGuard and removing redundant CanActivate implementation

--- src/common/guards/global-auth.guard.ts
+++ src/common/guards/global-auth.guard.ts
@@ -1,11 +1,14 @@
-import { Injectable, CanActivate, ExecutionContext } from '@nestjs/common';
+import { Injectable, ExecutionContext } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
+import { AuthGuard } from '@nestjs/passport';
 
 @Injectable()
-export class GlobalAuthGuard implements CanActivate {
-  constructor(private reflector: Reflector) {}
+export class GlobalAuthGuard extends AuthGuard('jwt') {
+  constructor(private reflector: Reflector) {
+    super();
+  }
 
-  canActivate(context: ExecutionContext): boolean {
+  canActivate(context: ExecutionContext) {
     const isPublic = this.reflector.getAllAndOverride<boolean>('isPublic', [
       context.getHandler(),
       context.getClass(),
@@ -15,7 +18,6 @@ export class GlobalAuthGuard implements CanActivate {
       return true;
     }
 
-    const request = context.switchToHttp().getRequest();
-    return !!request.user;
+    return super.canActivate(context);
   }
 }

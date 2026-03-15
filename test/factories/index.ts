b1e55a52a9bd91be071985709a8afa0c6cfcee6e
Erick <erickjo10x@gmail.com>
2026-03-15 12:09:42 +0100

    feat: add ScanLogFactory for generating scan log data

---
+++ test/factories/index.ts
@@ -0,0 +1,3 @@
+export { UserFactory } from './user.factory';
+export { QrCodeFactory } from './qr-code.factory';
+export { ScanLogFactory } from './scan-log.factory';
---
+++ test/factories/scan-log.factory.ts
@@ -0,0 +1,23 @@
+import { faker } from '@faker-js/faker/.';
+import { RecordScanDto } from 'src/modules/analytics/dto/record-scan.dto';
+
+export class ScanLogFactory {
+  static create(overrides?: Partial<RecordScanDto>): RecordScanDto {
+    return {
+      qrId: faker.number.int({ min: 1, max: 100 }),
+      userId:
+        Math.random() < 0.5 ? faker.number.int({ min: 1, max: 10 }) : undefined,
+      ipAddress: faker.internet.ipv4(),
+      userAgent: faker.internet.userAgent(),
+      ...overrides,
+    };
+  }
+
+  static createBatch(count: number): RecordScanDto[] {
+    return Array.from({ length: count }, () => this.create());
+  }
+
+  static createFromQrId(qrId: number, count: number): RecordScanDto[] {
+    return Array.from({ length: count }, () => this.create({ qrId }));
+  }
+}
